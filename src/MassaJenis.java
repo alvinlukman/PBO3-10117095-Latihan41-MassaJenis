bceca6f4784f4dd763c616038840b297bb37a54b
alvinlukmanNH <galinx20gmail.com>
2018-10-22 22:28:15 +0700

    massajenis

---
+++ src/MassaJenis.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * NAMA  :Alvin Lukman Nulhakim
+ * NIM   :10117095    
+ * KELAS :IF-3
+ */
+public class MassaJenis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Kubus objK = new Kubus();
+        System.out.println("==== Massa Jenis Kubus====");
+        objK.setSisi(5);
+        objK.setMassa(250);
+        System.out.println("Sisi : "+objK.getSisi());
+        System.out.println("Massa : "+objK.getMassa());
+        System.out.println("");
+        System.out.println("==== Hasil Perhitungan====");
+        System.out.println("Volume : "+objK.hitungVolume(0));
+        System.out.println("Massa Jenis : "+objK.hitungMassaJenis(0,0));
+    }
+    
+}
